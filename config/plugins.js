19ea5e4916416ffcae50c9aa80f3d96ebba338d9
Emre Simsek <emresimseka@gmail.com>
2024-03-18 15:46:10 +0300

    Vercel config

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_7DPbhUW9JrSjAV2y4ISfq589bQYx/kUmlVUkxoF",
+      apiToken: "NaxBCvopTTxBqX0QCOHlbq4o",
+      appFilter: "strapi-my-portfolio",
+      teamFilter: "team_DH9di7mmdt7kHIixxrIWiGBs",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
